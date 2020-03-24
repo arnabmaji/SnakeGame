a32d857f5e43565328d09d23aad359c552e4ae48
Arnab Maji <arnabmaji1999@gmail.com>
2020-03-24 19:24:11 +0530

    move snake grid according to directions

--- core/src/io/github/arnabmaji19/snakegame/Snake.java
+++ core/src/io/github/arnabmaji19/snakegame/Snake.java
@@ -6,16 +6,13 @@ public class Snake {
 
     private static final Texture texture = new Texture("snake.png");
     private static final int MAX_SNAKE_POSITION_CHANGE_COUNT = 8;
-    private static final float VELOCITY = 10.0f;
-
-    private float xPos;
-    private float yPos;
+    private static final float MAX_VELOCITY = 20.0f;
+    private static float velocity = MAX_VELOCITY;
+    private float xPos = 10.0f;
     private int snakePositionChangeCount = 0;
+    private float yPos = 10.0f;
+    private boolean isMovingVertically = false;
 
-    public Snake() {
-        this.xPos = 10.0f;
-        this.yPos = 10.0f;
-    }
 
     public static Texture getTexture() {
         return texture;
@@ -24,9 +21,41 @@ public class Snake {
     public void move() {
         if (snakePositionChangeCount > MAX_SNAKE_POSITION_CHANGE_COUNT) {  // change snake position in a delay
             snakePositionChangeCount = 0;
-            xPos += VELOCITY;  // increase snake's positions
+            // move position according to its direction
+            if (isMovingVertically) yPos += velocity; // increase snake's x position
+            else xPos += velocity;  // increase snake's y position
+
         } else snakePositionChangeCount++;
 
+    }
+
+    private boolean isMovingForward() {
+        return velocity > 0;
+    }
+
+    public void toggleDirection(boolean forward, boolean upward) {
+        // change snake direction: forward or backward
+        if (forward) velocity = MAX_VELOCITY;
+        else velocity = (-MAX_VELOCITY);
+
+        // change direction: upward or downward
+        isMovingVertically = upward;
+    }
+
+    public boolean isTowardsPositiveXAxis() {
+        return !isMovingVertically && isMovingForward();
+    }
+
+    public boolean isTowardsNegativeXAxis() {
+        return !isMovingVertically && !isMovingForward();
+    }
+
+    public boolean isTowardsPositiveYAxis() {
+        return isMovingVertically && isMovingForward();
+    }
+
+    public boolean isTowardsNegativeYAxis() {
+        return isMovingVertically && !isMovingForward();
     }
 
     public float getXPos() {
--- core/src/io/github/arnabmaji19/snakegame/SnakeGame.java
+++ core/src/io/github/arnabmaji19/snakegame/SnakeGame.java
@@ -1,6 +1,8 @@
 package io.github.arnabmaji19.snakegame;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -27,6 +29,29 @@ public class SnakeGame extends ApplicationAdapter {
 		snake.move();
 		// draw snake
 		batch.draw(Snake.getTexture(), snake.getXPos(), snake.getYPos());
+
+		// listen for key presses
+		if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+			if (snake.isTowardsPositiveXAxis() || snake.isTowardsNegativeXAxis())
+				snake.toggleDirection(true, true);  // make it towards positive y axis
+
+		} else if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+			if (snake.isTowardsPositiveXAxis() || snake.isTowardsNegativeXAxis())
+				snake.toggleDirection(false, true);  // make it towards negative y axis
+
+		} else if (Gdx.input.isKeyJustPressed(Input.Keys.LEFT)) {
+
+			if (snake.isTowardsPositiveYAxis() || snake.isTowardsNegativeYAxis()) {
+				snake.toggleDirection(false, false);  // make it towards negative x axis
+			}
+
+		} else if (Gdx.input.isKeyJustPressed(Input.Keys.RIGHT)) {
+			// if snake is towards positive Y-axis
+			if (snake.isTowardsPositiveYAxis() || snake.isTowardsNegativeYAxis()) {
+				snake.toggleDirection(true, false);  // make it towards positive x axis
+			}
+		}
+
 		batch.end();
 	}
 
